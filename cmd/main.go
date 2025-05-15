c605a6c78ddd2dcdfbf4b2e854338a4549ad995f
SlayerSv <slayer-sv@mail.ru>
2025-05-15 22:21:45 +0700

    feat: add graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/SlayerSv/load-balancer/internal/config"
 	"github.com/SlayerSv/load-balancer/internal/loadbalancer"
@@ -27,8 +31,24 @@ func main() {
 		Handler: lb,
 	}
 	slog.Info("Server starting", "port", cfg.Port)
-	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		slog.Error("Server failed", "error", err)
-		os.Exit(1)
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			slog.Error("Server failed", "error", err)
+			os.Exit(1)
+		}
+	}()
+	// Handle graceful shutdown
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+	<-sigChan
+	slog.Info("Received shutdown signal, shutting down...")
+
+	// Shutdown server with a timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("Server shutdown failed", "error", err)
+	} else {
+		slog.Info("Server shutdown successfully")
 	}
 }
